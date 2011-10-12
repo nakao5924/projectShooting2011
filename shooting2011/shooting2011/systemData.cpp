415d7a9d94d00d88f5fd3d5bb5a391174be036c6
Tianyao Li <li.t.ab@m.titech.ac.jp>
2011-10-12 18:43:33 +0900

    sysData11Oct

---
+++ shooting2011/shooting2011/systemData.cpp
@@ -0,0 +1,37 @@
+
+#include "systemData.h"
+
+SystemData::SystemData() {
+	COLOR_BLACK = GetColor(0, 0, 0);
+	COLOR_GREY = GetColor(128, 128, 128);
+	COLOR_WHITE = GetColor(255, 255, 255);
+}
+
+void SystemData::addHero() {
+	lifes.push_back(1);
+	scores.push_back(0);
+}
+
+void SystemData::addLife(int heroIndex, int dLife)
+{
+	lifes[heroIndex] += dLife;
+}
+
+void SystemData::addScore(int heroIndex, int dScore)
+{
+	scores[heroIndex] += dScore;
+}
+
+
+void SystemData::draw() {
+	int color = COLOR_WHITE;
+
+	DrawBox(SIDE_WIDTH, 0, SIDE_WIDTH + STAGE_WIDTH, STAGE_HEIGHT, COLOR_WHITE, FALSE);
+
+	for(int i=0; i<(int)lifes.size(); i++) {
+			DrawString(textPositions[i].lifePosX0, textPositions[i].lifePosY0, "Life:", color);
+			DrawFormatString(textPositions[i].lifePosX1, textPositions[i].lifePosY1, color, "%02d", scores[i]);
+			DrawString(textPositions[i].scorePosX0, textPositions[i].scorePosY0, "Score:", color);
+			DrawFormatString(textPositions[i].scorePosX1, textPositions[i].scorePosY1, color, "%08d", lifes[i]);
+	}
+}
---
+++ shooting2011/shooting2011/systemData.h
@@ -0,0 +1,46 @@
+
+#ifndef __SYSTEMDATA_H__
+#define __SYSTEMDATA_H__
+
+#include<vector>
+using namespace std;
+
+#include "movingObject.h"
+
+struct textPosition{
+  int lifePosX0;
+  int lifePosY0;
+  int lifePosX1;
+  int lifePosY1;
+
+  int scorePosX0;
+  int scorePosY0;
+  int scorePosX1;
+  int scorePosY1;
+};
+
+const textPosition textPositions[] = {
+  {10, 10, 170, 30, 10, 50, 120, 70},
+  {10, 490, 170, 510, 10, 550, 120, 570},
+  {610, 10, 770, 30, 610, 50, 720, 70},
+  {610, 490, 770, 510, 610, 550, 720, 570},
+};
+
+
+class SystemData {
+	vector<int> lifes;
+	vector<int> scores;
+
+public:
+	int COLOR_BLACK;
+	int COLOR_GREY;
+	int COLOR_WHITE;
+
+	SystemData();
+	void addHero();
+	void addScore(int heroIndex, int dScore);
+	void addLife(int heroIndex, int dLife);
+	void draw();
+};
+
+#endif
