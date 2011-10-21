9d5a8455664f3078feb2be9912eb7edbbee5379f
unknown <egawa@.ja1yad.local>
2011-10-21 18:15:02 +0900

    modified PictureTable to load animation data.

--- shooting2011/shooting2011/pictureTable.h
+++ shooting2011/shooting2011/pictureTable.h
@@ -9,19 +9,19 @@ using namespace std;
 class PictureTable{
 	//‚·‚×‚Ä‚Ì‰æ‘œ‚ğ“Ç‚İ‚ñ‚Å‚»‚Ìid‚ğŠÇ—‚·‚éB
 
-	map<int,int> decode_table;
+	map<int,deque<int>> decode_table;
 	map<string,int> graphic_encode_table;
-	map<int,pair<int,int>>half_size_table;//size‚Ì”¼•ª‚Ì‘å‚«‚³‚ªŠi”[‚³‚ê‚Ä‚¢‚éB
+	map<int,pair<int, int>>half_size_table;//size‚Ì”¼•ª‚Ì‘å‚«‚³‚ªŠi”[‚³‚ê‚Ä‚¢‚éB
 	void load_all_graphics(string str);
 public:
 	~PictureTable(){InitGraph();}
 	PictureTable(){}
 	int getID(string str);
 	void initialize(){load_all_graphics("../graphic/");}
-	int getanimation(int graphicID,int frameCount);
+	int getanimation(int tableIdx,int animIdx);
 	int gethalfsize_x(int graphicID){return half_size_table[graphicID].first;}
 	int gethalfsize_y(int graphicID){return half_size_table[graphicID].second;}
-	int decode(int in){return decode_table[in];}
+	int decode(int in){return decode_table[in][0];}
 	int encode(string str){return graphic_encode_table[str];}
 };
 
